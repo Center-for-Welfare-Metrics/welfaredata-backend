e14085eaa477e07935e724a262858fc16bdfb0bc
Herikle Mesquita <herikle.mesquita@gmail.com>
2020-12-31 10:36:03 -0300

    same site none | cross site cookies

--- bin/server.js
+++ bin/server.js
@@ -4,12 +4,22 @@ const express = require('express')
 const http = require('http')
 const middlewaresAppConfig = require('./config/middlewares')
 const routes = require('../src/routes')
+const session = require('express-session')
 const app = express()
 
 
 middlewaresAppConfig(app)
 
 app.use(express.static(__dirname,{dotfiles:'allow'}))
+
+if(process.env.NODE_ENV === 'prod'){
+    app.use(session({
+        cookie:{
+            sameSite:'none',
+            secure:true
+        }
+    }))
+}
 
 routes(app)
 
