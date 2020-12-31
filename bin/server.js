bc7090719f3e0543e05507006df8c3e081529802
Herikle Mesquita <herikle.mesquita@gmail.com>
2020-12-31 10:54:13 -0300

    set cookie same site to none

--- bin/server.js
+++ bin/server.js
@@ -4,22 +4,13 @@ const express = require('express')
 const http = require('http')
 const middlewaresAppConfig = require('./config/middlewares')
 const routes = require('../src/routes')
-// const session = require('express-session')
+
 const app = express()
 
 
 middlewaresAppConfig(app)
 
 app.use(express.static(__dirname,{dotfiles:'allow'}))
-
-// if(process.env.NODE_ENV === 'prod'){
-//     app.use(session({
-//         cookie:{
-//             sameSite:'none',
-//             secure:true
-//         }
-//     }))
-// }
 
 routes(app)
 
--- helpers/auth/authentication.js
+++ helpers/auth/authentication.js
@@ -4,7 +4,7 @@ const signIn = (should_singin,user,response) => {
     if(should_singin){
         delete user.password
         const token = jwt.sign(user,process.env.SECRET,{expiresIn:'2h'})
-        response.cookie('token',token,{httpOnly:true}).status(200).json(user)
+        response.cookie('token',token,{httpOnly:true,sameSite:'none'}).status(200).json(user)
     }else{
         response.status(412).json({
             email:['Credenciais não encontradas.']
