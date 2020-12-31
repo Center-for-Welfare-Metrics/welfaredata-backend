f97eee9a5a1e64a617c9df6348a73ae89709bead
Herikle Mesquita <herikle.mesquita@gmail.com>
2020-12-31 11:05:05 -0300

    check node_env var and use secute policy for set cookie cross domain over https

--- helpers/auth/authentication.js
+++ helpers/auth/authentication.js
@@ -4,7 +4,14 @@ const signIn = (should_singin,user,response) => {
     if(should_singin){
         delete user.password
         const token = jwt.sign(user,process.env.SECRET,{expiresIn:'2h'})
-        response.cookie('token',token,{httpOnly:true,sameSite:'none',secure:true}).status(200).json(user)
+        let options = {
+            httpOnly:true
+        }
+        if(process.env.NODE_EV === 'prod'){
+            options.sameSite = 'none'
+            options.secure = true
+        }
+        response.cookie('token',token,options).status(200).json(user)
     }else{
         response.status(412).json({
             email:['Credenciais não encontradas.']
@@ -14,7 +21,14 @@ const signIn = (should_singin,user,response) => {
 
 
 const logOut = (response) => {
-    response.clearCookie('token').sendStatus(200)
+    let options = {
+        httpOnly:true
+    }
+    if(process.env.NODE_EV === 'prod'){
+        options.sameSite = 'none'
+        options.secure = true
+    }
+    response.clearCookie('token',options).sendStatus(200)
 }
 
 module.exports = {
