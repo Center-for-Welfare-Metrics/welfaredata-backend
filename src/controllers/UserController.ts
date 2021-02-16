9d8a9886f03af02f1956fd0aaa2076fd9e777f31
Herikle Mesquita <herikle.mesquita@gmail.com>
2021-02-16 11:02:43 -0300

    remove console log

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,7 +4,6 @@ import {Request,Response} from 'express'
 const UserController = {
     get: (request:Request,response:Response) => {
         let {auth_user} = request
-        console.log(auth_user)
         response.success(auth_user)
     }
 }
