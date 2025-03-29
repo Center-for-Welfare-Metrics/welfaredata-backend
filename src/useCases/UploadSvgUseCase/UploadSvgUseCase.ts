c4eca4c9baac1eec65ddadc3255f068bd123b837
Herikle Mesquita <herikle.mesquita@gmail.com>
2025-03-29 02:00:48 -0300

    create raster images of all elements

--- src/useCases/UploadSvgUseCase/UploadSvgUseCase.ts
+++ src/useCases/UploadSvgUseCase/UploadSvgUseCase.ts
@@ -1,7 +1,8 @@
 import { optimize } from "svgo";
 import fs from "fs";
 import { removeUnusedIdsPlugin } from "src/svgo/plugins/removeUnusedIds";
-import { sortSvgChildren } from "./utils/sortSvgChildren";
+import { rasterizeSvg, sortSvgChildren } from "./utils/sortSvgChildren";
+import path from "path";
 
 interface File {
   buffer: Buffer;
@@ -39,7 +40,28 @@ export class UploadSvgUseCase {
 
     const sortedSvgContent = await sortSvgChildren(optimizedSvgContent);
 
+    const allElements = await rasterizeSvg(sortedSvgContent, '[id*="--"]');
+    if (allElements.length > 0) {
+      const folderPath = path.resolve("images");
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+      }
+
+      for (const element of allElements) {
+        const fileName = `${element.id}.png`;
+        const filePath = path.join(folderPath, fileName);
+
+        const base64Data = element.dataUrl.replace(
+          /^data:image\/png;base64,/,
+          ""
+        );
+        fs.writeFileSync(filePath, base64Data, "base64");
+
+        console.log(`Image saved as ${filePath}`);
+      }
+    }
     fs.writeFileSync("optimized.svg", sortedSvgContent);
+
     return {
       message: "SVG file uploaded successfully",
     };
