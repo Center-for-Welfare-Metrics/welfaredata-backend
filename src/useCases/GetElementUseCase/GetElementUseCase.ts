4bff1628bf80f9d7154b11f868adcc4b6dd55ea0
Herikle Mesquita <herikle.mesquita@gmail.com>
2025-04-22 10:04:04 -0300

    get specie by pathname

--- src/useCases/GetElementUseCase/GetElementUseCase.ts
+++ src/useCases/GetElementUseCase/GetElementUseCase.ts
@@ -1,3 +1,4 @@
+import Specie from "@/src/models/Specie";
 import SvgElement, { ISvgElement } from "@/src/models/SvgElement";
 
 interface Params {
@@ -6,11 +7,21 @@ interface Params {
 
 export class GetElementUseCase {
   async execute(params: Params): Promise<ISvgElement[]> {
-    const { specie } = params;
+    const { specie: pathname } = params;
+
+    const specie = await Specie.findOne({
+      pathname: pathname.toLowerCase(),
+    });
+
+    const specieId = specie?._id;
+
+    if (!specieId) {
+      throw new Error("Specie not found");
+    }
 
     // Get all root elements for the specified specie
     const rootElements = await SvgElement.find({
-      specie: specie.toLowerCase(),
+      specie_id: specieId,
       element_type: "root",
     });
 
