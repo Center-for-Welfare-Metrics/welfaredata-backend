d4db11a6aa106f8accfda6e1bbc0520cbd7e6973
Herikle Mesquita <herikle.mesquita@gmail.com>
2025-04-23 09:26:35 -0300

    update launch args

--- src/useCases/ElementUseCase/CreateElementUseCase/utils/puppeteerLaunchArgs.ts
+++ src/useCases/ElementUseCase/CreateElementUseCase/utils/puppeteerLaunchArgs.ts
@@ -3,6 +3,17 @@ import { LaunchOptions } from "puppeteer";
 export const puppeteerLaunchArgs: LaunchOptions = {
   headless: true,
   executablePath: process.env.BROWSER_EXECUTABLE_PATH,
-  args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+  args: [
+    "--disable-gpu",
+    "--disable-dev-shm-usage",
+    "--disable-setuid-sandbox",
+    "--no-first-run",
+    "--no-sandbox",
+    "--no-zygote",
+    "--single-process",
+    "--proxy-server='direct://'",
+    "--proxy-bypass-list=*",
+    "--deterministic-fetch",
+  ],
   dumpio: true,
 };
