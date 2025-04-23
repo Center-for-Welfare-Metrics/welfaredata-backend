d898e111573b52c39ed40b72afc7348566647e38
Herikle Mesquita <herikle.mesquita@gmail.com>
2025-04-23 08:28:37 -0300

    enable dumpio

--- src/useCases/ElementUseCase/CreateElementUseCase/utils/puppeteerLaunchArgs.ts
+++ src/useCases/ElementUseCase/CreateElementUseCase/utils/puppeteerLaunchArgs.ts
@@ -4,4 +4,5 @@ export const puppeteerLaunchArgs: LaunchOptions = {
   headless: true,
   executablePath: process.env.BROWSER_EXECUTABLE_PATH,
   args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+  dumpio: true,
 };
